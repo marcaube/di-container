8eb13867394ab3b8ea5ff41e01bf27b23e144b6a
marcaube <marcaube@users.noreply.github.com>
2015-08-29 17:45:35 -0400

    Spec a dependency injection container

---
+++ spec/ContainerSpec.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace spec\Ob\Di;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ContainerSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Ob\Di\Container');
+    }
+
+    function it_can_be_constructed_with_parameters()
+    {
+        $this->beConstructedWith([
+            'foo' => 'bar',
+            'baz' => 'qux',
+        ]);
+
+        $this->getParam('foo')->shouldEqual('bar');
+        $this->getParam('baz')->shouldEqual('qux');
+    }
+
+    function it_can_set_a_parameter()
+    {
+        $this->setParam('param', 'foo');
+
+        $this->getParam('param')->shouldEqual('foo');
+    }
+
+    function it_can_check_if_a_parameter_exists()
+    {
+        $this->hasParam('param')->shouldBe(false);
+    }
+
+    function it_can_unset_a_parameter()
+    {
+        $this->setParam('param', 'foo');
+        $this->hasParam('param')->shouldBe(true);
+
+        $this->unsetParam('param');
+        $this->hasParam('param')->shouldBe(false);
+    }
+
+    function it_can_register_services()
+    {
+        $this->set('service', function () {
+            return new \stdClass();
+        });
+
+        $this->get('service')->shouldHaveType('\stdClass');
+    }
+
+    function it_can_check_if_a_service_exists()
+    {
+        $this->has('service')->shouldReturn(false);
+
+        $this->set('service', function () {
+            return new \stdClass();
+        });
+
+        $this->has('service')->shouldReturn(true);
+    }
+
+    function it_should_keep_instances_of_created_services()
+    {
+        $this->set('service', function () {
+            return new \stdClass();
+        });
+
+        // Get a service
+        $service = $this->getWrappedObject()->get('service');
+
+        // Get it again and make sure it's the same instance
+        $this->get('service')->shouldReturn($service);
+    }
+
+    function it_can_redefine_a_service()
+    {
+        $this->set('service', function () {
+            return new \stdClass();
+        });
+
+        $service = $this->getWrappedObject()->get('service');
+
+        $this->set('service', function () {
+            return new \stdClass();
+        });
+
+        // Make sure it's not the same instance
+        $this->get('service')->shouldHaveType('\stdClass');
+        $this->get('service')->shouldNotReturn($service);
+    }
+}
---
+++ src/Container.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Ob\Di;
+
+class Container
+{
+    /**
+     * Container parameters.
+     *
+     * @var array
+     */
+    private $parameters = [];
+
+    /**
+     * A registry of callables to create services.
+     *
+     * @var array
+     */
+    private $callables = [];
+
+    /**
+     * A registry of service instance.
+     *
+     * @var array
+     */
+    private $instances = [];
+
+    /**
+     * @param array $parameters An associative array of parameters (i.e. ['name' => $value])
+     */
+    public function __construct($parameters = [])
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Set a container parameter.
+     *
+     * @param string $name  The parameter name
+     * @param mixed  $value The parameter value
+     */
+    public function setParam($name, $value)
+    {
+        $this->parameters[$name] = $value;
+    }
+
+    /**
+     * Retrieve a container parameter.
+     *
+     * @param string $name The parameter name
+     *
+     * @return mixed
+     */
+    public function getParam($name)
+    {
+        return $this->parameters[$name];
+    }
+
+    /**
+     * Return whether a container parameter exists or not.
+     *
+     * @param string $name The parameter name
+     *
+     * @return bool
+     */
+    public function hasParam($name)
+    {
+        return isset($this->parameters[$name]);
+    }
+
+    /**
+     * Unset a container parameter.
+     *
+     * @param string $name The parameter name
+     */
+    public function unsetParam($name)
+    {
+        unset($this->parameters[$name]);
+    }
+
+    /**
+     * Register a callable to create a service by name.
+     *
+     * @param string   $name     The service name
+     * @param callable $callable A callable that returns an object.
+     */
+    public function set($name, $callable)
+    {
+        $this->callables[$name] = $callable;
+        unset($this->instances[$name]);
+    }
+
+    /**
+     * Get a service by name.
+     *
+     * @param string $name The service name
+     *
+     * @return mixed
+     */
+    public function get($name)
+    {
+        if (!isset($this->instances[$name])) {
+            $this->instances[$name] = call_user_func($this->callables[$name]);
+        }
+
+        return $this->instances[$name];
+    }
+
+    /**
+     * Return whether a service exists or not.
+     *
+     * @param string $name The service name
+     *
+     * @return bool
+     */
+    public function has($name)
+    {
+        return isset($this->callables[$name]);
+    }
+}
